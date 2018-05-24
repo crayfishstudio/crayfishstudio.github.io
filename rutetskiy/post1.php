10ccdb31a4a3c29fd9f04eb9ccdf1559733fb39c
AndriySotnyk <AndriySotnuk@gmail.com>
2018-05-24 23:55:29 +0300

    callbcak service
    callback service via php added

---
+++ rutetskiy/post1.php
@@ -0,0 +1,44 @@
+<? 
+// ----------------------------конфигурация-------------------------- // 
+ 
+$adminemail="info@rutetskiy.com";  // e-mail админа 
+ 
+$date=date("d.m.y"); // число.месяц.год 
+$time=date("H:i"); // часы:минуты:секунды 
+  // На какую страничку переходит после отправки письма 
+ 
+//---------------------------------------------------------------------- // 
+ 
+// Принимаем данные с формы 
+ 
+$name=$_POST['user_name']; 
+$phone=$_POST['user_phone']; 
+$email=$_POST['user_email'];
+$adres=$_POST['user_adres'];
+
+// Проверяем валидность e-mail 
+ 
+
+
+$msg=" 
+Ім'я: $name
+Телефон: $phone 
+Email: $email
+Адрес: $adres
+"; 
+
+ // Отправляем письмо админу  
+mail("$adminemail", "$date $time Повідомлення 
+від $name", "$msg");  
+ 
+// Выводим сообщение пользователю 
+ 
+print "<script language='Javascript'><!-- 
+function reload() {location.replace('http://rutetskiy.com')}; setTimeout('reload()', 6000); 
+//--></script> 
+ 
+$msg 
+ 
+<p>Повідомлення відправленно, скоро тут буде щось цікавіше (або ні)...</p>"; 
+exit; 
+?>
