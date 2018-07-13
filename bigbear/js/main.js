28c966847cee18ff88739a06bb3aac2aac41e468
crayfishstudio <37799171+crayfishstudio@users.noreply.github.com>
2018-07-14 00:49:27 +0300

    breadcrumb test

--- bigbear/js/main.js
+++ bigbear/js/main.js
@@ -375,3 +375,17 @@ $(window).resize(function() {
     var inputwidth = $('#div-datepicker').width() + "px";
     $('.datepicker').css("width", inputwidth);  
 });
+
+var lastScrollTop = 0;
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+   var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+   if (st > lastScrollTop){
+      console.log("scrolldown");
+       document.getElementById("breadcrumb").classList.add("hided");
+   } else {
+       console.log("scrollup");
+      document.getElementById("breadcrumb").classList.remove("hided");
+   }
+   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}, false);
