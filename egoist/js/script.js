4d8a22538a318ff4db622012f8e6e3fb9ac32695
Andriy Sotnyk <34285859+AndriySotnyk@users.noreply.github.com>
2018-08-12 23:25:24 +0300

    fixes

---
+++ egoist/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.slickCarousel').slick({
+        accessibility: true,
+        autoplay: true,
+        arrows: true,
+        autoplaySpeed: 30000
+    });
+});
