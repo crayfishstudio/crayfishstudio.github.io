dc6691af0995f5c7f01d277ea8375bb460167ab6
volodymyrleha <33492547+volodymyrleha@users.noreply.github.com>
2018-08-26 03:48:37 +0300

    slider

--- egoist/js/script.js
+++ egoist/js/script.js
@@ -2,7 +2,10 @@ $(document).ready(function(){
     $('.slickCarousel').slick({
         accessibility: true,
         autoplay: true,
+        adaptiveHeight: true,
         arrows: true,
+        nextArrow: $('#right-arrow'),
+        prevArrow: $('#left-arrow'),
         autoplaySpeed: 30000
     });
     ($('.types').height($('.card-body').height()));
