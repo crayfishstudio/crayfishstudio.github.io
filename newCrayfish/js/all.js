e5da62e33d96e9fb194512c9acc64a6eee9ebcc7
crayfishstudio <37799171+crayfishstudio@users.noreply.github.com>
2018-10-19 21:57:48 +0300

    commit

--- newCrayfish/js/all.js
+++ newCrayfish/js/all.js
@@ -3,3 +3,59 @@ $(document).ready(function(){
 		$(this).toggleClass('open');
 	});
 });
+
+var lastScrollTop = 0;
+$(window).on('scroll', function() {
+    st = $(this).scrollTop();
+    if(st < lastScrollTop) {
+        document.getElementById("menuToggle").classList.remove("hided");
+    }
+    else {
+        document.getElementById("menuToggle").classList.add("hided");
+    }
+    lastScrollTop = st;
+});
+
+$(document).ready(function(){
+  $('.first-lane').delay(50000).slick({
+      accessibility: false,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      arrows: false,
+      cssEase: "cubic-bezier(0.77,0.2,0.05,1.0)",
+      speed: 1000,
+      pauseOnFocus: false,
+      pauseOnHover: false,
+      swipe: false,
+      touchMove: false,
+      vertical: true
+  });
+    
+    $('.second-lane').slick({
+      accessibility: false,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      arrows: false,
+      cssEase: "cubic-bezier(0.77,0.2,0.05,1.0)",
+      speed: 1000,
+      pauseOnFocus: false,
+      pauseOnHover: false,
+      swipe: false,
+      touchMove: false,
+      vertical: true
+  });
+    
+    $('.third-lane').slick({
+      accessibility: false,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      arrows: false,
+      cssEase: "cubic-bezier(0.77,0.2,0.05,1.0)",
+      speed: 1000,
+      pauseOnFocus: false,
+      pauseOnHover: false,
+      swipe: false,
+      touchMove: false,
+      vertical: true
+  });
+});
