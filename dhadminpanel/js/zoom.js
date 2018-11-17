2dc85b87443d85fc6ed763cdda641c7533c04918
Andriy Sotnyk <34285859+AndriySotnyk@users.noreply.github.com>
2018-11-17 17:39:10 +0200

    fix

--- dhadminpanel/js/zoom.js
+++ dhadminpanel/js/zoom.js
@@ -1,264 +1,278 @@
-/*!
- * zoom.js 0.3
- * http://lab.hakim.se/zoom-js
- * MIT licensed
- *
- * Copyright (C) 2011-2014 Hakim El Hattab, http://hakim.se
- */
-var zoom = (function(){
++function ($) { "use strict";
 
-	var TRANSITION_DURATION = 800;
+  /**
+   * The zoom service
+   */
+  function ZoomService () {
+    this._activeZoom            =
+    this._initialScrollPosition =
+    this._initialTouchPosition  =
+    this._touchMoveListener     = null
 
-	// The current zoom level (scale)
-	var level = 1;
+    this._$document = $(document)
+    this._$window   = $(window)
+    this._$body     = $(document.body)
 
-	// The current mouse position, used for panning
-	var mouseX = 0,
-		mouseY = 0;
+    this._boundClick = $.proxy(this._clickHandler, this)
+  }
 
-	// Timeout before pan is activated
-	var panEngageTimeout = -1,
-		panUpdateInterval = -1;
+  ZoomService.prototype.listen = function () {
+    this._$body.on('click', '[data-action="zoom"]', $.proxy(this._zoom, this))
+  }
 
-	// Timeout for callback function
-	var callbackTimeout = -1;
+  ZoomService.prototype._zoom = function (e) {
+    var target = e.target
 
-	// Check for transform support so that we can fallback otherwise
-	var supportsTransforms = 	'WebkitTransform' in document.body.style ||
-								'MozTransform' in document.body.style ||
-								'msTransform' in document.body.style ||
-								'OTransform' in document.body.style ||
-								'transform' in document.body.style;
+    if (!target || target.tagName != 'IMG') return
 
-	if( supportsTransforms ) {
-		// The easing that will be applied when we zoom in/out
-		document.body.style.transition = 'transform '+ TRANSITION_DURATION +'ms ease';
-		document.body.style.OTransition = '-o-transform '+ TRANSITION_DURATION +'ms ease';
-		document.body.style.msTransition = '-ms-transform '+ TRANSITION_DURATION +'ms ease';
-		document.body.style.MozTransition = '-moz-transform '+ TRANSITION_DURATION +'ms ease';
-		document.body.style.WebkitTransition = '-webkit-transform '+ TRANSITION_DURATION +'ms ease';
-	}
+    if (this._$body.hasClass('zoom-overlay-open')) return
 
-	// Zoom out if the user hits escape
-	document.addEventListener( 'keyup', function( event ) {
-		if( level !== 1 && event.keyCode === 27 ) {
-			zoom.out();
-		}
-	} );
+    if (e.metaKey || e.ctrlKey) {
+      return window.open((e.target.getAttribute('data-original') || e.target.src), '_blank')
+    }
 
-	// Monitor mouse movement for panning
-	document.addEventListener( 'mousemove', function( event ) {
-		if( level !== 1 ) {
-			mouseX = event.clientX;
-			mouseY = event.clientY;
-		}
-	} );
+    if (target.width >= ($(window).width() - Zoom.OFFSET)) return
 
-	/**
-	 * Applies the CSS required to zoom in, prefers the use of CSS3
-	 * transforms but falls back on zoom for IE.
-	 *
-	 * @param {Object} rect
-	 * @param {Number} scale
-	 */
-	function magnify( rect, scale ) {
+    this._activeZoomClose(true)
 
-		var scrollOffset = getScrollOffset();
+    this._activeZoom = new Zoom(target)
+    this._activeZoom.zoomImage()
 
-		// Ensure a width/height is set
-		rect.width = rect.width || 1;
-		rect.height = rect.height || 1;
+    // todo(fat): probably worth throttling this
+    this._$window.on('scroll.zoom', $.proxy(this._scrollHandler, this))
 
-		// Center the rect within the zoomed viewport
-		rect.x -= ( window.innerWidth - ( rect.width * scale ) ) / 2;
-		rect.y -= ( window.innerHeight - ( rect.height * scale ) ) / 2;
+    this._$document.on('keyup.zoom', $.proxy(this._keyHandler, this))
+    this._$document.on('touchstart.zoom', $.proxy(this._touchStart, this))
 
-		if( supportsTransforms ) {
-			// Reset
-			if( scale === 1 ) {
-				document.body.style.transform = '';
-				document.body.style.OTransform = '';
-				document.body.style.msTransform = '';
-				document.body.style.MozTransform = '';
-				document.body.style.WebkitTransform = '';
-			}
-			// Scale
-			else {
-				var origin = scrollOffset.x +'px '+ scrollOffset.y +'px',
-					transform = 'translate('+ -rect.x +'px,'+ -rect.y +'px) scale('+ scale +')';
+    // we use a capturing phase here to prevent unintended js events
+    // sadly no useCapture in jquery api (http://bugs.jquery.com/ticket/14953)
+    if (document.addEventListener) {
+      document.addEventListener('click', this._boundClick, true)
+    } else {
+      document.attachEvent('onclick', this._boundClick, true)
+    }
 
-				document.body.style.transformOrigin = origin;
-				document.body.style.OTransformOrigin = origin;
-				document.body.style.msTransformOrigin = origin;
-				document.body.style.MozTransformOrigin = origin;
-				document.body.style.WebkitTransformOrigin = origin;
+    if ('bubbles' in e) {
+      if (e.bubbles) e.stopPropagation()
+    } else {
+      // Internet Explorer before version 9
+      e.cancelBubble = true
+    }
+  }
 
-				document.body.style.transform = transform;
-				document.body.style.OTransform = transform;
-				document.body.style.msTransform = transform;
-				document.body.style.MozTransform = transform;
-				document.body.style.WebkitTransform = transform;
-			}
-		}
-		else {
-			// Reset
-			if( scale === 1 ) {
-				document.body.style.position = '';
-				document.body.style.left = '';
-				document.body.style.top = '';
-				document.body.style.width = '';
-				document.body.style.height = '';
-				document.body.style.zoom = '';
-			}
-			// Scale
-			else {
-				document.body.style.position = 'relative';
-				document.body.style.left = ( - ( scrollOffset.x + rect.x ) / scale ) + 'px';
-				document.body.style.top = ( - ( scrollOffset.y + rect.y ) / scale ) + 'px';
-				document.body.style.width = ( scale * 100 ) + '%';
-				document.body.style.height = ( scale * 100 ) + '%';
-				document.body.style.zoom = scale;
-			}
-		}
+  ZoomService.prototype._activeZoomClose = function (forceDispose) {
+    if (!this._activeZoom) return
 
-		level = scale;
-	}
+    if (forceDispose) {
+      this._activeZoom.dispose()
+    } else {
+      this._activeZoom.close()
+    }
 
-	/**
-	 * Pan the document when the mouse cursor approaches the edges
-	 * of the window.
-	 */
-	function pan() {
-		var range = 0.12,
-			rangeX = window.innerWidth * range,
-			rangeY = window.innerHeight * range,
-			scrollOffset = getScrollOffset();
+    this._$window.off('.zoom')
+    this._$document.off('.zoom')
 
-		// Up
-		if( mouseY < rangeY ) {
-			window.scroll( scrollOffset.x, scrollOffset.y - ( 1 - ( mouseY / rangeY ) ) * ( 14 / level ) );
-		}
-		// Down
-		else if( mouseY > window.innerHeight - rangeY ) {
-			window.scroll( scrollOffset.x, scrollOffset.y + ( 1 - ( window.innerHeight - mouseY ) / rangeY ) * ( 14 / level ) );
-		}
+    document.removeEventListener('click', this._boundClick, true)
 
-		// Left
-		if( mouseX < rangeX ) {
-			window.scroll( scrollOffset.x - ( 1 - ( mouseX / rangeX ) ) * ( 14 / level ), scrollOffset.y );
-		}
-		// Right
-		else if( mouseX > window.innerWidth - rangeX ) {
-			window.scroll( scrollOffset.x + ( 1 - ( window.innerWidth - mouseX ) / rangeX ) * ( 14 / level ), scrollOffset.y );
-		}
-	}
+    this._activeZoom = null
+  }
 
-	function getScrollOffset() {
-		return {
-			x: window.scrollX !== undefined ? window.scrollX : window.pageXOffset,
-			y: window.scrollY !== undefined ? window.scrollY : window.pageYOffset
-		}
-	}
+  ZoomService.prototype._scrollHandler = function (e) {
+    if (this._initialScrollPosition === null) this._initialScrollPosition = $(window).scrollTop()
+    var deltaY = this._initialScrollPosition - $(window).scrollTop()
+    if (Math.abs(deltaY) >= 40) this._activeZoomClose()
+  }
 
-	return {
-		/**
-		 * Zooms in on either a rectangle or HTML element.
-		 *
-		 * @param {Object} options
-		 *
-		 *   (required)
-		 *   - element: HTML element to zoom in on
-		 *   OR
-		 *   - x/y: coordinates in non-transformed space to zoom in on
-		 *   - width/height: the portion of the screen to zoom in on
-		 *   - scale: can be used instead of width/height to explicitly set scale
-		 *
-		 *   (optional)
-		 *   - callback: call back when zooming in ends
-		 *   - padding: spacing around the zoomed in element
-		 */
-		to: function( options ) {
+  ZoomService.prototype._keyHandler = function (e) {
+    if (e.keyCode == 27) this._activeZoomClose()
+  }
 
-			// Due to an implementation limitation we can't zoom in
-			// to another element without zooming out first
-			if( level !== 1 ) {
-				zoom.out();
-			}
-			else {
-				options.x = options.x || 0;
-				options.y = options.y || 0;
+  ZoomService.prototype._clickHandler = function (e) {
+    if (e.preventDefault) e.preventDefault()
+    else event.returnValue = false
 
-				// If an element is set, that takes precedence
-				if( !!options.element ) {
-					// Space around the zoomed in element to leave on screen
-					var padding = typeof options.padding === 'number' ? options.padding : 20;
-					var bounds = options.element.getBoundingClientRect();
+    if ('bubbles' in e) {
+      if (e.bubbles) e.stopPropagation()
+    } else {
+      // Internet Explorer before version 9
+      e.cancelBubble = true
+    }
 
-					options.x = bounds.left - padding;
-					options.y = bounds.top - padding;
-					options.width = bounds.width + ( padding * 2 );
-					options.height = bounds.height + ( padding * 2 );
-				}
+    this._activeZoomClose()
+  }
 
-				// If width/height values are set, calculate scale from those values
-				if( options.width !== undefined && options.height !== undefined ) {
-					options.scale = Math.max( Math.min( window.innerWidth / options.width, window.innerHeight / options.height ), 1 );
-				}
+  ZoomService.prototype._touchStart = function (e) {
+    this._initialTouchPosition = e.touches[0].pageY
+    $(e.target).on('touchmove.zoom', $.proxy(this._touchMove, this))
+  }
 
-				if( options.scale > 1 ) {
-					options.x *= options.scale;
-					options.y *= options.scale;
+  ZoomService.prototype._touchMove = function (e) {
+    if (Math.abs(e.touches[0].pageY - this._initialTouchPosition) > 10) {
+      this._activeZoomClose()
+      $(e.target).off('touchmove.zoom')
+    }
+  }
 
-					options.x = Math.max( options.x, 0 );
-					options.y = Math.max( options.y, 0 );
 
-					magnify( options, options.scale );
+  /**
+   * The zoom object
+   */
+  function Zoom (img) {
+    this._fullHeight      =
+    this._fullWidth       =
+    this._overlay         =
+    this._targetImageWrap = null
 
-					if( options.pan !== false ) {
+    this._targetImage = img
 
-						// Wait with engaging panning as it may conflict with the
-						// zoom transition
-						panEngageTimeout = setTimeout( function() {
-							panUpdateInterval = setInterval( pan, 1000 / 60 );
-						}, TRANSITION_DURATION );
+    this._$body = $(document.body)
+  }
 
-					}
+  Zoom.OFFSET = 80
+  Zoom._MAX_WIDTH = 2560
+  Zoom._MAX_HEIGHT = 4096
 
-					if( typeof options.callback === 'function' ) {
-						callbackTimeout = setTimeout( options.callback, TRANSITION_DURATION );
-					}
-				}
-			}
-		},
+  Zoom.prototype.zoomImage = function () {
+    var img = document.createElement('img')
+    img.onload = $.proxy(function () {
+      this._fullHeight = Number(img.height)
+      this._fullWidth = Number(img.width)
+      this._zoomOriginal()
+    }, this)
+    img.src = this._targetImage.src
+  }
 
-		/**
-		 * Resets the document zoom state to its default.
-		 *
-		 * @param {Object} options
-		 *   - callback: call back when zooming out ends
-		 */
-		out: function( options ) {
-			clearTimeout( panEngageTimeout );
-			clearInterval( panUpdateInterval );
-			clearTimeout( callbackTimeout );
+  Zoom.prototype._zoomOriginal = function () {
+    this._targetImageWrap           = document.createElement('div')
+    this._targetImageWrap.className = 'zoom-img-wrap'
 
-			magnify( { x: 0, y: 0 }, 1 );
+    this._targetImage.parentNode.insertBefore(this._targetImageWrap, this._targetImage)
+    this._targetImageWrap.appendChild(this._targetImage)
 
-			if( options && typeof options.callback === 'function' ) {
-				setTimeout( options.callback, TRANSITION_DURATION );
-			}
+    $(this._targetImage)
+      .addClass('zoom-img')
+      .attr('data-action', 'zoom-out')
 
-			level = 1;
-		},
+    this._overlay           = document.createElement('div')
+    this._overlay.className = 'zoom-overlay'
 
-		// Alias
-		magnify: function( options ) { this.to( options ) },
-		reset: function() { this.out() },
+    document.body.appendChild(this._overlay)
 
-		zoomLevel: function() {
-			return level;
-		}
-	}
+    this._calculateZoom()
+    this._triggerAnimation()
+  }
 
-})();
+  Zoom.prototype._calculateZoom = function () {
+    this._targetImage.offsetWidth // repaint before animating
 
+    var originalFullImageWidth  = this._fullWidth
+    var originalFullImageHeight = this._fullHeight
+
+    var scrollTop = $(window).scrollTop()
+
+    var maxScaleFactor = originalFullImageWidth / this._targetImage.width
+
+    var viewportHeight = ($(window).height() - Zoom.OFFSET)
+    var viewportWidth  = ($(window).width() - Zoom.OFFSET)
+
+    var imageAspectRatio    = originalFullImageWidth / originalFullImageHeight
+    var viewportAspectRatio = viewportWidth / viewportHeight
+
+    if (originalFullImageWidth < viewportWidth && originalFullImageHeight < viewportHeight) {
+      this._imgScaleFactor = maxScaleFactor
+
+    } else if (imageAspectRatio < viewportAspectRatio) {
+      this._imgScaleFactor = (viewportHeight / originalFullImageHeight) * maxScaleFactor
+
+    } else {
+      this._imgScaleFactor = (viewportWidth / originalFullImageWidth) * maxScaleFactor
+    }
+  }
+
+  Zoom.prototype._triggerAnimation = function () {
+    this._targetImage.offsetWidth // repaint before animating
+
+    var imageOffset = $(this._targetImage).offset()
+    var scrollTop   = $(window).scrollTop()
+
+    var viewportY = scrollTop + ($(window).height() / 2)
+    var viewportX = ($(window).width() / 2)
+
+    var imageCenterY = imageOffset.top + (this._targetImage.height / 2)
+    var imageCenterX = imageOffset.left + (this._targetImage.width / 2)
+
+    this._translateY = viewportY - imageCenterY
+    this._translateX = viewportX - imageCenterX
+
+    var targetTransform = 'scale(' + this._imgScaleFactor + ')'
+    var imageWrapTransform = 'translate(' + this._translateX + 'px, ' + this._translateY + 'px)'
+
+    if ($.support.transition) {
+      imageWrapTransform += ' translateZ(0)'
+    }
+
+    $(this._targetImage)
+      .css({
+        '-webkit-transform': targetTransform,
+            '-ms-transform': targetTransform,
+                'transform': targetTransform
+      })
+
+    $(this._targetImageWrap)
+      .css({
+        '-webkit-transform': imageWrapTransform,
+            '-ms-transform': imageWrapTransform,
+                'transform': imageWrapTransform
+      })
+
+    this._$body.addClass('zoom-overlay-open')
+  }
+
+  Zoom.prototype.close = function () {
+    this._$body
+      .removeClass('zoom-overlay-open')
+      .addClass('zoom-overlay-transitioning')
+
+    // we use setStyle here so that the correct vender prefix for transform is used
+    $(this._targetImage)
+      .css({
+        '-webkit-transform': '',
+            '-ms-transform': '',
+                'transform': ''
+      })
+
+    $(this._targetImageWrap)
+      .css({
+        '-webkit-transform': '',
+            '-ms-transform': '',
+                'transform': ''
+      })
+
+    if (!$.support.transition) {
+      return this.dispose()
+    }
+
+    $(this._targetImage)
+      .one($.support.transition.end, $.proxy(this.dispose, this))
+      .emulateTransitionEnd(300)
+  }
+
+  Zoom.prototype.dispose = function () {
+    if (this._targetImageWrap && this._targetImageWrap.parentNode) {
+      $(this._targetImage)
+        .removeClass('zoom-img')
+        .attr('data-action', 'zoom')
+
+      this._targetImageWrap.parentNode.replaceChild(this._targetImage, this._targetImageWrap)
+      this._overlay.parentNode.removeChild(this._overlay)
+
+      this._$body.removeClass('zoom-overlay-transitioning')
+    }
+  }
+
+  // wait for dom ready (incase script included before body)
+  $(function () {
+    new ZoomService().listen()
+  })
+
+}(jQuery)
