2e87aad1f3b8f58a7732dc348ec3e329d91d9476
Andriy Sotnyk <34285859+AndriySotnyk@users.noreply.github.com>
2018-12-24 21:34:54 +0200

    large amount of notjing (fields, functions, fixes and etc.)

--- dhadminpanel/js/script.js
+++ dhadminpanel/js/script.js
@@ -1,6 +1,9 @@
 // When the user scrolls the page, execute myFunction 
 
-window.onscroll = function() {myFunction();scrollFunction();};
+window.onscroll = function () {
+    myFunction();
+    scrollFunction();
+};
 
 
 // Get the header
@@ -11,11 +14,11 @@ var sticky = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
 }
 // When the user scrolls down 20px from the top of the document, show the button
 
@@ -33,45 +36,66 @@ function toTop() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-function bText(){
 
-    document.body.style.fontSize="0.9rem";
-}
-function nText(){
+function bText() {
 
-    document.body.style.fontSize="0.8rem";
-}
-function sText(){
-
-    document.body.style.fontSize="0.7rem";
+    document.body.style.fontSize = "0.9rem";
 }
 
-function saveShow(){
+function nText() {
+
+    document.body.style.fontSize = "0.75rem";
+}
+
+function sText() {
+
+    document.body.style.fontSize = "0.65rem";
+}
+
+function saveShow() {
     document.getElementById("classification").innerHTML = document.getElementById("classification-area").value;
-    document.getElementById("classification-area").value="";
-    document.getElementById("classification-area").style.display="none";
+    document.getElementById("classification-area").value = "";
+    document.getElementById("classification-area").style.display = "none";
 }
 
 var autoExpand = function (field) {
 
-	// Reset field height
-	field.style.height = '1rem';
+    // Reset field height
+    field.style.height = '1rem';
 
-	// Get the computed styles for the element
-	var computed = window.getComputedStyle(field);
+    // Get the computed styles for the element
+    var computed = window.getComputedStyle(field);
 
-	// Calculate the height
-	var height = parseInt(computed.getPropertyValue('border-top-width'), 10)
-	             + parseInt(computed.getPropertyValue('padding-top'), 10)
-	             + field.scrollHeight
-	             + parseInt(computed.getPropertyValue('padding-bottom'), 10)
-	             + parseInt(computed.getPropertyValue('border-bottom-width'), 10);
+    // Calculate the height
+    var height = parseInt(computed.getPropertyValue('border-top-width'), 10) +
+        parseInt(computed.getPropertyValue('padding-top'), 10) +
+        field.scrollHeight +
+        parseInt(computed.getPropertyValue('padding-bottom'), 10) +
+        parseInt(computed.getPropertyValue('border-bottom-width'), 10);
 
-	field.style.height = height + 'px';
+    field.style.height = height + 'px';
 
 };
 
 document.addEventListener('input', function (event) {
-	if (event.target.tagName.toLowerCase() !== 'textarea') return;
-	autoExpand(event.target);
+    if (event.target.tagName.toLowerCase() !== 'textarea') return;
+    autoExpand(event.target);
 }, false);
+//$(".fa-question-circle").hover(
+//    function(){
+//        $(this).parent().addClass("helper");
+//}, function(){
+//       setTimeout(function(){
+//           $(this).parent().removeClass("helper");
+//       }, 3000);
+//});
+function addCoAuth(){
+    var $coAuthTr = $(".co-auth").html();
+    $(".co-auth").last().after("<tr class='row co-auth'>" + $coAuthTr + "</tr>");
+};
+//$(".addCoAuth").click(function(){
+//    var $coAuthTr = $(this).parent().parent().parent().html();
+//    console.log($coAuthTr);
+//    console.log($("co-auth").html());
+//    $(this).parent().parent().parent().after($coAuthTr);
+//})
