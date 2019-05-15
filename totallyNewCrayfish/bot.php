5f15687d35891835f86cd00bbd01517ecde256ed
crayfishstudio <37799171+crayfishstudio@users.noreply.github.com>
2019-05-15 13:38:46 +0300

    formbot

---
+++ totallyNewCrayfish/bot.php
@@ -0,0 +1,12 @@
+define ('url',"https://api.telegram.org/bot713508432:AAHv1XghRc6DKweDtWmcOMKaXojZEh2mmfg/");
+
+$name = $_POST['userName'];
+$contacts = $_POST['userContact'];
+
+$message = $_POST['message'];
+
+$chat_id = '-309131950';
+
+$message = urlencode("Name: $name\nContacts: $contacts\n$Message : $message");
+
+file_get_contents(url."sendmessage?text=$message&chat_id=$chat_id&parse_mode=HTML");
