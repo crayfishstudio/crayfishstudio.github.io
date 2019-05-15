a6d94cfa118c26fc09d950dc20f59dff466d4470
crayfishstudio <37799171+crayfishstudio@users.noreply.github.com>
2019-05-15 14:30:11 +0300

    Final index

--- totallyNewCrayfish/bot.php
+++ totallyNewCrayfish/bot.php
@@ -1,3 +1,4 @@
+<?php
 define ('url',"https://api.telegram.org/bot713508432:AAHv1XghRc6DKweDtWmcOMKaXojZEh2mmfg/");
 
 $name = $_POST['userName'];
@@ -10,3 +11,6 @@ $chat_id = '-309131950';
 $message = urlencode("Name: $name\nContacts: $contacts\n$Message : $message");
 
 file_get_contents(url."sendmessage?text=$message&chat_id=$chat_id&parse_mode=HTML");
+
+redirect("thanks.html");
+?>
