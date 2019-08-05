999bd443a6a58d03408f2243d8ac038545d8f158
Anton Kitsera <antonkitsera@gmail.com>
2019-08-05 12:59:29 +0300

    Update

--- knife/js/script.js
+++ knife/js/script.js
@@ -1,7 +1,7 @@
 let socialBlock = document.querySelector(".social");
 let socialLink = document.querySelectorAll(".social__link");
 
-let anchorsArr = ['#home', '#blade-style', '#blade-finish', '#handle', '#size', '#generate', '#mail', '#final'];
+let anchorsArr = ['#home', '#blade-style', '#blade-finish', '#handle', '#size', '#generate', '#mail', '#final', '#devby'];
 
 let dynamic = () => {
     if(window.location.hash == anchorsArr[0]) {
@@ -9,51 +9,67 @@ let dynamic = () => {
             el.style.transition = "opacity 0.5s linear 0s";
             el.style.opacity = 1;
         });
+        socialBlock.style.zIndex = 1;
 
     } else if(window.location.hash == anchorsArr[1]) {
         socialLink.forEach(el => {
             el.style.transition = "opacity 0.5s linear 0s";
             el.style.opacity = 0.5;
         });
+        socialBlock.style.zIndex = 1;
 
     } else if(window.location.hash == anchorsArr[2]) {
         socialLink.forEach(el => {
             el.style.transition = "opacity 0.5s linear 0s";
             el.style.opacity = 0.5;
         });
+        socialBlock.style.zIndex = 1;
 
     } else if(window.location.hash == anchorsArr[3]) {
         socialLink.forEach(el => {
             el.style.transition = "opacity 0.5s linear 0s";
             el.style.opacity = 0.5;
         });
+        socialBlock.style.zIndex = 1;
 
     } else if(window.location.hash == anchorsArr[4]) {
         socialLink.forEach(el => {
             el.style.transition = "opacity 0.5s linear 0s";
             el.style.opacity = 0.5;
         });
+        socialBlock.style.zIndex = 1;
 
     } else if(window.location.hash == anchorsArr[5]) {
         socialLink.forEach(el => {
             el.style.transition = "opacity 0s linear 0s";
             el.style.opacity = 0;
         });
+        socialBlock.style.zIndex = 1;
 
     } else if(window.location.hash == anchorsArr[6]) {
         socialLink.forEach(el => {
             el.style.transition = "opacity 0s linear 0s";
             el.style.opacity = 0;
         });
+        socialBlock.style.zIndex = 1;
 
     } else if(window.location.hash == anchorsArr[7]) {
         socialLink.forEach(el => {
             el.style.transition = "opacity 0.5s linear 0s";
             el.style.opacity = 0.5;
         });
+        socialBlock.style.zIndex = 1;
+
+    } else if(window.location.hash == anchorsArr[8]) {
+        socialLink.forEach(el => {
+            el.style.transition = "opacity 0.5s linear 0s";
+            el.style.opacity = 0.5;
+        });
+        socialBlock.style.zIndex = 0;
     }
 }
 
+/*
 let dynamicMobile = () => {
     if(document.documentElement.clientWidth <= 768) {
         if(window.location.hash != anchorsArr[7]) {
@@ -79,12 +95,12 @@ let dynamicMobileWidth = () => {
         }
     }
 
-}
+}*/
 
 let start = () => {
     dynamic();
-    dynamicMobile();
-    dynamicMobileWidth();
+    /*dynamicMobile();
+    dynamicMobileWidth();*/
 }
 
 let radioButton = document.querySelectorAll(".section-item__radio");
@@ -93,6 +109,6 @@ for(var i =0; i < radioButton.length; i++) {
     radioButton[i].onclick = function() {
         setTimeout(function() {
             fullpage_api.moveSectionDown(); 
-        }, 400);
+        }, 200);
     };
 }
