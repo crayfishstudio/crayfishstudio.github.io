68389ac3d7a4875fef23907f4a4dccb0071fd418
volodymyrleha <33492547+volodymyrleha@users.noreply.github.com>
2019-08-25 03:26:03 +0300

    Catalog page fixed

--- maxpower/js/script.js
+++ maxpower/js/script.js
@@ -44,12 +44,107 @@ $(document).ready(function () {
     $('#footer-form-phone').mask('+00 (000) 000 0000');
 
     $('.block').click(function() {
-    $(this).find('.info').slideToggle(500);
+        $(this).find('.info').slideToggle(500);
 
-    if ($(this).hasClass('active-block')) {
-        $(this).removeClass('active-block');
-    } else {
-        $(this).addClass('active-block');
-    }
-  });
+        if ($(this).hasClass('active-block')) {
+            $(this).removeClass('active-block');
+        } else {
+            $(this).addClass('active-block');
+        }
+      });
+
+    $('.input-popup').click(function(event) {
+        if (event.target.className == "input-popup-arrow") {
+            if ($(this).hasClass('input-popup-active')) {
+                $(this).removeClass('input-popup-active');
+
+                $(this).parent().find('.input-popup-content').find('.label-2').each(function() {
+                    if ($(this).find('input').prop("checked") == true) {
+                        $(this).find('input').prop("checked", false);
+                    }
+                });
+
+                $(this).find('span').text("");
+
+                $(this).parent().find("#number_btn").removeClass("button-active");
+                $(this).parent().find("#min-range-number").prop("disabled", true);
+                $(this).parent().find("#max-range-number").prop("disabled", true);
+
+            } else {
+                $('.input-popup').not($(this)).each(function() {
+                    if ($(this).hasClass('input-popup-open')) {
+                        $(this).removeClass('input-popup-open');
+                        $(this).parent().find('.input-popup-content').fadeToggle(300);
+                    }
+                });
+
+                if ($(this).hasClass('input-popup-open')) {
+                    $(this).removeClass('input-popup-open');
+                } else {
+                    $(this).addClass('input-popup-open');
+                }
+
+                $(this).parent().find('.input-popup-content').fadeToggle(300);
+            }
+
+        } else {
+            $('.input-popup').not($(this)).each(function() {
+                if ($(this).hasClass('input-popup-open')) {
+                    $(this).removeClass('input-popup-open');
+                    $(this).parent().find('.input-popup-content').fadeToggle(300);
+                }
+            });
+
+            if ($(this).hasClass('input-popup-open')) {
+                $(this).removeClass('input-popup-open');
+            } else {
+                $(this).addClass('input-popup-open');
+            }
+
+            $(this).parent().find('.input-popup-content').fadeToggle(300);
+        }
+    });
+
+
+    $('.input-popup-content').find('p').click(function() {
+        var radio = "";
+        $('.input-popup-radio').find('p').text($(this).text());
+
+        radio += $(this).attr("to");
+        $(radio).prop('checked', true);     
+    });
+
+    $('.checkmark').click(function() {
+        $(this).parent().find('input').prop("checked", true);
+    });
+
+    $('.label').click(function() {
+        if ($(this).find('input').prop("checked") == true) {
+            $(this).find('input').prop("checked", false);
+        } else {
+            $(this).find('input').prop("checked", true);
+        }
+
+        var n = 0;
+
+        $(this).parent().find('.label-2').each(function() {
+            if ($(this).find('input').prop("checked") == true) {
+                if (n == 0) {
+                    n = 1;
+                }
+            }
+        });
+
+        if (n == 1) {
+            $(this).parent().parent().find('.input-popup').addClass('input-popup-active');
+        } else {
+            $(this).parent().parent().find('.input-popup').removeClass('input-popup-active');
+        }        
+    });
+
+    $('.radio-popup').click(function() {
+        $(this).fadeToggle(300);
+        $(this).parent().find('.input-popup').removeClass('input-popup-open');
+    });
+
 });
