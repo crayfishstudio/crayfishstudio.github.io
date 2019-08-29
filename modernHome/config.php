518da6fc826c2101e23f5fe94fc0991af13abde4
Malendento <50326927+Malendento@users.noreply.github.com>
2019-08-29 23:04:16 +0300

    МАЙНКРАФТ ЭТО МАЯ ЖИИИИИЗНЬ БЛЯДЬ
    МАЙНКРААААААААААААААААААФТ

--- modernHome/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DBHOST', 'localhost');
-define('DBUSER', 'chmoder9245');
-define('DBPASS', 'KvEAmzDM');
-define('DBNAME', 'chmoder9245');
-$db = mysqli_connect(DBHOST, DBUSER, DBPASS,DBNAME);
-mysqli_set_charset($db, "utf8");
-
--- modernHome/delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once('config.php');
-$stmt = "DELETE FROM etaps WHERE id=".$_GET["id"];
-$db->query($stmt);
-mysqli_close($db);
-header('Location: admin.php');
