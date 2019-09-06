d52bc0a0f00819147057889c139345f37fca15ff
Anton Kitsera <antonkitsera@gmail.com>
2019-09-06 17:22:10 +0300

    Update script.js

--- school/js/script.js
+++ school/js/script.js
@@ -213,3 +213,9 @@ $('body').on('click', function(e) {
 		$('.select-header').removeClass('select-open');
 	}
 });
+
+var ua = navigator.userAgent.toLowerCase();
+var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
+if(isAndroid) {
+	alert("hello");
+}
