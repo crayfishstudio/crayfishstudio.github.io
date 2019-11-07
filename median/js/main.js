f848b0702fec1006e17e7a7fb0d11e00002ad558
Malendento <50326927+Malendento@users.noreply.github.com>
2019-11-07 15:40:53 +0200

    for-yura

---
+++ median/js/main.js
@@ -0,0 +1,25 @@
+        $(document).on('click', 'a[href^="#"]', function (event) {
+            event.preventDefault();
+
+            $('html, body').animate({
+                scrollTop: $($.attr(this, 'href')).offset().top - 120
+            }, 900);
+        });
+
+        $(document).ready(function () {
+            $('.sidebar-search__anchors a, .nav__chrested, .mobile-sidebar__logo, .sidebar-search__language a').on('click', function () {
+                $('.sidebar__mobile').removeClass('active-sidebar');
+            });
+
+            $('.sidebar-search__anchors a, .mobile-sidebar__logo, .sidebar-search__language a').on('click', function () {
+                $('.hamburger').removeClass('nav__chrested');
+            });
+
+            $('.hamburger').click(function () {
+                $('.sidebar__mobile').toggleClass('active-sidebar');
+            });
+
+            $('.hamburger').click(function () {
+                $(this).toggleClass('nav__chrested');
+            });
+        });
