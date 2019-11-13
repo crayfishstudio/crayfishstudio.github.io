82d3d40f69c860a3efd2724f3ba6575f62ba3da0
Malendento <50326927+Malendento@users.noreply.github.com>
2019-11-13 04:42:51 +0200

    fixes

--- kraftEats/js/main.js
+++ kraftEats/js/main.js
@@ -30,3 +30,9 @@
                     mask: '+{38}(000)000-00-00'
                 });
         });
+
+const phoneMask = IMask(
+    document.getElementById('your-id'), {
+      mask: '+{00}(000)000-00-00',
+    },
+  );
