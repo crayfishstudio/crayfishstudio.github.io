746c739c421bb0781da1475db4dd23155454d13d
Yurii Skarbek <yurii.skarbek@gmail.com>
2019-12-30 18:50:12 +0200

    Added animation for generation new knife

--- knife/js/gener.js
+++ knife/js/gener.js
@@ -13,17 +13,17 @@ BLADE STYLE - 1
 
 let bladeStyleGroup = document.querySelectorAll('input[name="blade-style"]');
 
-for(let i = 0; i < bladeStyleGroup.length; i++) {
-  bladeStyleGroup[i].addEventListener('click', function() {
-    	getBladeStyleCheckedValue();
+for (let i = 0; i < bladeStyleGroup.length; i++) {
+  bladeStyleGroup[i].addEventListener('click', function () {
+    getBladeStyleCheckedValue();
   });
 }
 
 let getBladeStyleCheckedValue = (bladeStyleChecked) => {
-	bladeStyleChecked = document.querySelector('input[name="blade-style"]:checked').value;
-  
+  bladeStyleChecked = document.querySelector('input[name="blade-style"]:checked').value;
+
   paramGroup[0] = +bladeStyleChecked;
-	console.log(...paramGroup);
+  console.log(...paramGroup);
 }
 
 /* 
@@ -34,17 +34,17 @@ BLADE FINISH - 2
 
 let bladeFinishGroup = document.querySelectorAll('input[name="blade-finish"]');
 
-for(let i = 0; i < bladeFinishGroup.length; i++) {
-  bladeFinishGroup[i].addEventListener('click', function() {
-    	getBladeFinishCheckedValue();
+for (let i = 0; i < bladeFinishGroup.length; i++) {
+  bladeFinishGroup[i].addEventListener('click', function () {
+    getBladeFinishCheckedValue();
   });
 }
 
 let getBladeFinishCheckedValue = (bladeFinishChecked) => {
-	bladeFinishChecked = document.querySelector('input[name="blade-finish"]:checked').value;
-  
+  bladeFinishChecked = document.querySelector('input[name="blade-finish"]:checked').value;
+
   paramGroup[1] = +bladeFinishChecked;
-	console.log(...paramGroup);
+  console.log(...paramGroup);
 }
 
 /* 
@@ -55,17 +55,17 @@ HANDLE - 3
 
 let handleGroup = document.querySelectorAll('input[name="handle"]');
 
-for(let i = 0; i < handleGroup.length; i++) {
-  handleGroup[i].addEventListener('click', function() {
-    	getHandleCheckedValue();
+for (let i = 0; i < handleGroup.length; i++) {
+  handleGroup[i].addEventListener('click', function () {
+    getHandleCheckedValue();
   });
 }
 
 let getHandleCheckedValue = (handleChecked) => {
-	handleChecked = document.querySelector('input[name="handle"]:checked').value;
-  
+  handleChecked = document.querySelector('input[name="handle"]:checked').value;
+
   paramGroup[2] = +handleChecked;
-	console.log(...paramGroup);
+  console.log(...paramGroup);
 }
 
 /* 
@@ -76,18 +76,18 @@ SIZE - 4
 
 let sizeGroup = document.querySelectorAll('input[name="size"]');
 
-for(let i = 0; i < sizeGroup.length; i++) {
-  sizeGroup[i].addEventListener('click', function() {
-    	getSizeCheckedValue();
+for (let i = 0; i < sizeGroup.length; i++) {
+  sizeGroup[i].addEventListener('click', function () {
+    getSizeCheckedValue();
   });
 }
 
 let getSizeCheckedValue = (sizeChecked) => {
-	sizeChecked = document.querySelector('input[name="size"]:checked').value;
-  
+  sizeChecked = document.querySelector('input[name="size"]:checked').value;
+
   paramGroup[3] = +sizeChecked;
-	console.log(...paramGroup);
-  
+  console.log(...paramGroup);
+
   let paramGroupString = paramGroup.join('');
 
   console.log(paramGroupString);
@@ -98,29 +98,71 @@ let getSizeCheckedValue = (sizeChecked) => {
 let generateButton = document.getElementById('generate__button');
 let generateNumber = document.getElementById('generate__number');
 let generateGif = document.getElementById('generate-block');
+let generateTitle = document.getElementById('generate__title');
+let generateCounters = document.getElementById('final-counters');
+let generateBlock = document.getElementById('generate-block');
 
+let finalCount1 = document.getElementById('final-count1')
+let finalCount2 = document.getElementById('final-count2')
+let finalCount3 = document.getElementById('final-count3')
+let finalCount4 = document.getElementById('final-count4')
 
-generateButton.addEventListener('click', function() {
-    
-    let paramGroupString = paramGroup.join('');
-    
-    if(paramGroup[0] == 0) {
-        console.log("1 - is empty");
-        fullpage_api.moveTo(2);
-    } else if(paramGroup[1] == 0) {
-        console.log("2 - is empty");
-        fullpage_api.moveTo(3);
-    } else if(paramGroup[2] == 0) {
-        console.log("3 - is empty");
-        fullpage_api.moveTo(4);
-    } else if(paramGroup[3] == 0) {
-        console.log("4 - is empty");
-        fullpage_api.moveTo(5);
-    } else {
-        console.log("all okay");
-        console.log('generateButton: ', paramGroupString);
-        generateNumber.innerHTML = paramGroupString;
-        // change background gif 
-        generateGif.style.backgroundImage = "url('img/knifes/" + paramGroupString + ".gif')";
+generateButton.addEventListener('click', function () {
+  let paramGroupString = paramGroup.join('');
+
+  if (paramGroup[0] == 0) {
+    console.log("1 - is empty");
+    fullpage_api.moveTo(2);
+  } else if (paramGroup[1] == 0) {
+    console.log("2 - is empty");
+    fullpage_api.moveTo(3);
+  } else if (paramGroup[2] == 0) {
+    console.log("3 - is empty");
+    fullpage_api.moveTo(4);
+  } else if (paramGroup[3] == 0) {
+    console.log("4 - is empty");
+    fullpage_api.moveTo(5);
+  } else {
+    console.log("all okay");
+    console.log('generateButton: ', paramGroupString);
+
+    //fading bg
+    generateBlock.style.opacity = '0';
+
+    function changeKnife() {
+      // change background gif 
+      generateGif.style.backgroundImage = "url('img/knifes/" + paramGroupString + ".gif')";
+
+      // change title
+      generateTitle.innerHTML = 'THIS IS YOUR PERSONAL INERTIX CONFIGURATION';
+
+      // change button text
+      generate__button.innerHTML = 'GET YOUR PERSONAL CONFIGURATION';
+      generate__button.setAttribute('onclick', 'fullpage_api.moveTo(8)');
+
+      // show counters
+      generateCounters.style.left = '0';
+
+      // show counters
+      generateCounters.style.display = 'block';
+      if(paramGroup[3] == 1){
+        finalCount1.innerHTML = '100';
+        finalCount2.innerHTML = '50';
+        finalCount3.innerHTML = '4';
+        finalCount4.innerHTML = '130g';
+      } else if(paramGroup[3] == 2){
+        finalCount1.innerHTML = '150';
+        finalCount2.innerHTML = '50';
+        finalCount3.innerHTML = '4';
+        finalCount4.innerHTML = '217g';
+      } else{
+        generateCounters.style.display = 'none';
+      }
+      
+      setTimeout("generateBlock.style.opacity = '1';", 100);
     }
+
+    setTimeout(changeKnife, 500);
+
+  }
 });
